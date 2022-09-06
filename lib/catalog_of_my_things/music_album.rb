74eeebf48de9ccec84b0d5e31caa0e6818f54bf0
oyelakinG9 <oyelakinr37@gmail.com>
2022-09-06 18:23:14 +0100

    Create Music Album rb file

---
+++ lib/catalog_of_my_things/music_album.rb
@@ -0,0 +1,14 @@
+require './item'
+
+class Music < Item
+  attr_accessor :on_spotify
+
+  def initialize(publish_date, id = Random.rand(1..1_000_000), archived: false)
+    super(publish_date, id, archived: archived)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && on_spotify ? true || false
+  end
+end
