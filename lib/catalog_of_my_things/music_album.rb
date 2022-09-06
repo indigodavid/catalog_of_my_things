117231d6982f43835225975fe1ab11a15f1128c5
oyelakinG9 <oyelakinr37@gmail.com>
2022-09-06 21:16:34 +0100

    Modify music album rb file

--- lib/catalog_of_my_things/music_album.rb
+++ lib/catalog_of_my_things/music_album.rb
@@ -1,9 +1,9 @@
 require './item'
 
-class Music < Item
+class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(publish_date, id = Random.rand(1..1_000_000), archived: false)
+  def initialize(publish_date, on_spotify, id = Random.rand(1..1_000_000), archived: false)
     super(publish_date, id, archived: archived)
     @on_spotify = on_spotify
   end
