be345f84c85df20fa4403cfd6eb3fde0938f19dc
oyelakinG9 <oyelakinr37@gmail.com>
2022-09-06 21:52:35 +0100

    Create genre class method

--- lib/catalog_of_my_things/genre.rb
+++ lib/catalog_of_my_things/genre.rb
@@ -1,1 +1,15 @@
-require './item.rb'
+class Genre
+  attr_reader :items
+  attr_accessor :id, :name
+
+  def initialize(name, id = Random.rand(1..1_000_000))
+    @name = name
+    @id = id
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.genre = self
+  end
+end
