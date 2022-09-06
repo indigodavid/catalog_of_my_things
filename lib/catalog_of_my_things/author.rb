9d700900b8fa193ef88573c6c9d9ac89e58ffc84
David Vera Castillo <indigodavid@gmail.com>
2022-09-06 18:45:45 -0500

    Add id to constructor on Author class

--- lib/catalog_of_my_things/author.rb
+++ lib/catalog_of_my_things/author.rb
@@ -2,10 +2,10 @@ class Author
   attr_accessor :first_name, :last_name
   attr_reader :id, :items
 
-  def initialize(first_name, last_name)
+  def initialize(first_name, last_name, id = Random.rand(0..1_000_000))
     @first_name = first_name
     @last_name = last_name
-    @id = Random.rand(0..1_000_000)
+    @id = id
     @items = []
   end
 
