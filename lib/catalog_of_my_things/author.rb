2118db990e109df013d1421ee0efd8c8edf79082
David Vera Castillo <indigodavid@gmail.com>
2022-09-06 09:53:09 -0500

    Add Author class

---
+++ lib/catalog_of_my_things/author.rb
@@ -0,0 +1,15 @@
+class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :id, :items
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @id = Random.rand(0..1_000_000)
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
