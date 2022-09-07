1729065be522e868cd19e66107385c51901f25ec
David Vera Castillo <indigodavid@gmail.com>
2022-09-07 15:11:56 -0500

    Add methods to handle JSON behavior in Game class

--- lib/catalog_of_my_things/game.rb
+++ lib/catalog_of_my_things/game.rb
@@ -11,19 +11,35 @@ class Game < Item
   end
 
   def can_be_archived?
-    super && ((DateTime.now - @last_played_at) > 2)
+    super && ((DateTime.now - @last_played_at) / 365.25 > 2)
   end
 
-  def to_json(_options = nil)
-    JSON.pretty_generate({
-                           id: @id,
-                           publish_date: @publish_date,
-                           genre: @genre ? @genre.attributes : nil,
-                           author: @author ? @author.attributes : nil,
-                           label: @label ? @label.attributes : nil,
-                           archived: @archived,
-                           multiplayer: @multiplayer,
-                           last_played_at: @last_played_at
-                         })
+  def to_json(*args)
+    {
+      json.create_id => self.class.name,
+      'id' => @id,
+      'publish_date' => @publish_date,
+      'genre' => @genre ? @genre.to_json : nil,
+      'author' => @author ? @author.to_json : nil,
+      'label' => @label ? @label.to_json : nil,
+      'archived' => @archived,
+      'multiplayer' => @multiplayer,
+      'last_played_at' => @last_played_at
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    obj = new(
+      object['publish_date'],
+      object['multiplayer'],
+      object['last_played_at'],
+      object['id'],
+      archived: object['archived']
+    )
+    obj.add_author('author')
+    obj.add_genre('genre')
+    obj.add_label('label')
+
+    obj
   end
 end
