fe9f14a54ba32f9563321ad0bc1f2a44aa29c763
jevazquezb <je.vazquezb@gmail.com>
2022-09-07 15:05:08 -0500

    Delete writing-to-json-files methods and change name from retrieve to load

--- lib/catalog_of_my_things/book_json.rb
+++ lib/catalog_of_my_things/book_json.rb
@@ -2,21 +2,7 @@ require_relative './book'
 require 'json'
 
 module BooksJson
-  def save_books(books)
-    data = books.map do |book|
-      {
-        id: book.id,
-        publish_date: book.publish_date,
-        publisher: book.publisher,
-        cover_state: book.cover_state,
-        archived: book.archived
-      }
-    end
-
-    File.write('./lib/catalog_of_my_things/books.json', JSON.pretty_generate(data))
-  end
-
-  def retrieve_books
+  def load_books
     data = []
     file = './lib/catalog_of_my_things/books.json'
 
