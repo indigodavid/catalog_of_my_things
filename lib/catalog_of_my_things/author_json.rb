e98690d7a0be1e5e403a1dceb08061badf1fe5f2
David Vera Castillo <indigodavid@gmail.com>
2022-09-08 10:29:23 -0500

    Parse internal JSON objects on modules

--- lib/catalog_of_my_things/author_json.rb
+++ lib/catalog_of_my_things/author_json.rb
@@ -8,6 +8,7 @@ module AuthorJson
 
     if File.exist?(file) && File.read(file) != ''
       JSON.parse(File.read(file)).each do |author|
+        author = JSON.parse(author)
         data << Author.new(
           author['first_name'],
           author['last_name'],
